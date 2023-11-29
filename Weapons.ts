4ffda49a6c2a343f3ceb5deca5b635c8e9f4ebb7
Murilo <dixmurilox@gmail.com>
2023-11-29 15:48:04 -0300

    Feat adding method to increase Damage and Value

--- Weapons.ts
+++ Weapons.ts
@@ -1,72 +1,77 @@
 enum weaponRarity {
-    Comum,
-    Incomum,
-    Rare,
-    Epic,
-    Legendary,
+  Comum,
+  Incomum,
+  Rare,
+  Epic,
+  Legendary,
 }
 
 export class Weapon {
-    constructor(
-        private _name: string,
-        private _damage: number,
-        private _type: string,
-        private _value: number
-    ) { }
+  constructor(
+    private _name: string,
+    private _damage: number,
+    private _type: string,
+    private _value: number,
+  ) {}
 
-
-    defineRarity(damage: number): weaponRarity {
-        if (damage < 20) {
-            return weaponRarity.Comum;
-        } else if (damage >= 20 && damage < 50) {
-            return weaponRarity.Incomum;
-        } else if (damage >= 50 && damage < 60) {
-            return weaponRarity.Rare;
-        } else if (damage >= 60 && damage < 80) {
-            return weaponRarity.Epic;
-        } else if (damage > 80) {
-            return weaponRarity.Legendary;
-        } else {
-            return weaponRarity.Comum;
-        }
+  defineRarity(damage: number): weaponRarity {
+    if (damage < 20) {
+      return weaponRarity.Comum;
+    } else if (damage >= 20 && damage < 50) {
+      return weaponRarity.Incomum;
+    } else if (damage >= 50 && damage < 60) {
+      return weaponRarity.Rare;
+    } else if (damage >= 60 && damage < 80) {
+      return weaponRarity.Epic;
+    } else if (damage > 80) {
+      return weaponRarity.Legendary;
+    } else {
+      return weaponRarity.Comum;
     }
+  }
 
-    getRarityString() {
-        switch (this.defineRarity(this._damage)) {
-            case weaponRarity.Comum:
-                return "Comum";
-            case weaponRarity.Incomum:
-                return "Incomum";
-            case weaponRarity.Rare:
-                return "Rare";
-            case weaponRarity.Epic:
-                return "Epic";
-            case weaponRarity.Legendary:
-                return "Legendary";
-        }
+  getRarityString() {
+    switch (this.defineRarity(this._damage)) {
+      case weaponRarity.Comum:
+        return 'Comum';
+      case weaponRarity.Incomum:
+        return 'Incomum';
+      case weaponRarity.Rare:
+        return 'Rare';
+      case weaponRarity.Epic:
+        return 'Epic';
+      case weaponRarity.Legendary:
+        return 'Legendary';
     }
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    get damage() {
-        return this._damage;
-    }
+  get damage() {
+    return this._damage;
+  }
 
-    get type() {
-        return this._type;
-    }
+  get type() {
+    return this._type;
+  }
 
-    get raridade() {
-        return this.getRarityString();
-    }
+  get raridade() {
+    return this.getRarityString();
+  }
 
-    get value() {
-        return this._value;
-    }
+  get value() {
+    return this._value;
+  }
+
+  increaseDamage(amount: number): void {
+    this._damage += amount;
+  }
+  increaveValue(amount: number): void {
+    this._value += amount;
+  }
 }
 
-const espada = new Weapon("Espada", 50, "Corte", 100);
+const espada = new Weapon('Espada', 50, 'Corte', 100);
 espada.defineRarity(espada.damage);
-
