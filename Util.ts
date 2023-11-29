8a9f9fba1b44d7c791b0407c5ae64bb5f514c4d3
Murilo <dixmurilox@gmail.com>
2023-11-29 15:47:29 -0300

    Feat Util

--- Util.ts
+++ Util.ts
@@ -1,31 +1,117 @@
-import { Creator } from "./Creator";
-import { Stats } from "./Stats";   
-import { Enemy, Gladimir, Edecio, Bruna, Angelo, Fishman, lilAngel, Demon, Bandit, Vampire, Goblin, Troll, Fairy  } from "./Enemy";
+import { Creator } from './Creator';
+import { Stats } from './Stats';
+import { Weapon } from './Weapons';
+import {
+  Enemy,
+  Gladimir,
+  Edecio,
+  Bruna,
+  Angelo,
+  Fishman,
+  lilAngel,
+  Demon,
+  Bandit,
+  Vampire,
+  Goblin,
+  Troll,
+  Fairy,
+} from './Enemy';
 
-const prompt = require("prompt-sync")();
+const prompt = require('prompt-sync')();
 const write = prompt;
 
 let mobs: Enemy[] = [];
 let bosses: Enemy[] = [];
 
 // Aplicando mobs ao array mobs:
-mobs.push(new Goblin(), new Troll(), new Fairy(), new Bandit(), new Vampire(), new Fishman(), new lilAngel(), new Demon());
+mobs.push(
+  new Goblin(),
+  new Troll(),
+  new Fairy(),
+  new Bandit(),
+  new Vampire(),
+  new Fishman(),
+  new lilAngel(),
+  new Demon(),
+);
 
 // Aplicando os bosses ao array bosses:
 bosses.push(new Gladimir(), new Edecio(), new Bruna(), new Angelo());
 
+export class Inventory {
+  public items: Item[];
+  public gold: number;
 
-export class Util{
-    public stats: Stats;
+  constructor() {
+    this.items = [];
+    this.gold = 0;
+  }
 
-    constructor(){
-        this.stats = new Stats();
+  addItem(item: Item | Weapon): void {
+    this.items.push(item);
+  }
+
+  removeItem(item: Item | Weapon): void {
+    this.items = this.items.filter(i => i !== item);
+  }
+
+  hasItem(item: Item | Weapon): boolean {
+    return this.items.includes(item);
+  }
+
+  showInventory(): void {
+    console.log('Inventário:');
+    console.log('');
+    console.log('Itens:');
+    console.log('');
+    this.items.forEach(item => {
+      console.log(item.name);
+    });
+    console.log('');
+    console.log('Gold:');
+    console.log('');
+    console.log(this.gold);
+    console.log('');
+  }
+
+  sellItem(item: Item | Weapon): void {
+    this.removeItem(item);
+    this.gold += item.value;
+  }
+
+  checkItem(item: Item | Weapon): boolean {
+    return this.items.includes(item);
+  }
+
+  checkGold(item: Item | Weapon): boolean {
+    return this.gold >= item.value;
+  }
+
+  checkInventory(item: Item | Weapon): boolean {
+    return this.checkItem(item) && this.checkGold(item);
+  }
+
+  checkInventoryItem(item: Item | Weapon): boolean {
+    return this.checkItem(item);
+  }
+}
+
+export class Item {
+  constructor(public name: string, public value: number) {}
+}
+
+export class Util {
+  public stats: Stats;
+  public inventory: Inventory;
+  constructor() {
+    this.stats = new Stats();
+    this.inventory = new Inventory();
+  }
+
+  checkMaxHealth(creator: Creator): number {
+    if (creator.stats.health > creator.stats.max_health) {
+      creator.stats.health = creator.stats.max_health;
     }
-
-    checkMaxHealth(creator: Creator): number {
-        if (creator.stats.health > creator.stats.max_health) {
-            creator.stats.health = creator.stats.max_health
-        }
-        return creator.stats.health;
-    }
+    return creator.stats.health;
+  }
 }
