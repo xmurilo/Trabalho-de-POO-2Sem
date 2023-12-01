3612595d7b70bea326cd817119f1ae6b2abb7dee
Murilo <dixmurilox@gmail.com>
2023-12-01 15:16:18 -0300

    Feat Main Game

---
+++ Main.ts
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+const write = prompt;
+import { Creator } from "./Creator";
+import { Util } from "./Util";
+import { Blacksmith } from "./Contructions/Blacksmith";
