a6fb5f3da4cc156444008902f06c216e1691a59d
Lucas Mega Perez <lucas.megaperez@hotmail.com>
2023-12-01 15:56:55 -0300

    weapons

--- Weapons.ts
+++ Weapons.ts
@@ -73,5 +73,23 @@ export class Weapon {
   }
 }
 
-const espada = new Weapon('Espada', 50, 'Corte', 100);
-espada.defineRarity(espada.damage);
+const armas: Weapon[] = [
+  new Weapon('Arco Longo', 45, 'Perfuração', 80),
+  new Weapon('Maça de Guerra', 55, 'Impacto', 120),
+  new Weapon('Lança', 30, 'Perfuração', 50),
+  new Weapon('Machado de Batalha', 70, 'Corte', 100),
+  new Weapon('Florete', 35 , 'Perfuração', 60),
+  new Weapon('Besta', 40, 'Perfuração', 90),
+  new Weapon('Clava Espinhosa', 50, 'Impacto', 70),
+  new Weapon('Cajado de Curandeiro', 25, 'Magia', 110),
+  new Weapon('Espada Bastarda', 60, 'Corte', 95),
+  new Weapon('Flechas da Voracidade', 48, 'Perfuração', 75),
+]
+
+export const randomWeapon = (): Weapon => {
+  const randomIndex = Math.floor(Math.random() * armas.length);
+  return armas[randomIndex];
+}
+
+;
+
