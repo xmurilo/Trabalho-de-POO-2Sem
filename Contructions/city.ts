4433b823ae1f6682aa4f208549cc7d0a683e1554
Murilo <dixmurilox@gmail.com>
2023-11-30 22:04:47 -0300

    Feat separate class City of Blacksmith

--- Contructions/city.ts
+++ Contructions/city.ts
@@ -1,12 +1,9 @@
-import { Creator } from '../Creator';
-import { Item } from '../Util';
-import { Stats } from '../Stats';
-import { Util } from '../Util';
-import { Weapon } from '../Weapons';
-import { Quest } from '../Quests/quests';
-
 const prompt = require('prompt-sync')();
 const write = prompt;
+import { Creator } from '../Creator';
+import { Util } from '../Util';
+
+
 
 export abstract class City {
   public creator: Creator;
@@ -20,157 +17,158 @@ export abstract class City {
   public abstract services(creator: Creator): void;
 }
 
-export class Blacksmith extends City {
-  public services(creator: Creator): void {
-    console.log('Bem vindo a loja do ferreiro');
-    console.log(`Você tem: ${creator.gold} de ouro`);
-    console.log('O que deseja fazer?');
-    console.log('1 - Comprar');
-    console.log('2 - Vender');
-    console.log('3 - Melhorar equipamento');
-    console.log('4 - Sair');
+// export class Blacksmith extends City {
+//   public services(creator: Creator): void {
+//     console.log('Bem vindo a loja do ferreiro');
+//     console.log(`Você tem: ${creator.gold} de ouro`);
+//     console.log('O que deseja fazer?');
+//     console.log('1 - Comprar');
+//     console.log('2 - Vender');
+//     console.log('3 - Melhorar equipamento');
+//     console.log('4 - Sair');
 
-    let option = write('Digite a opção: ');
+//     let option = write('Digite a opção: ');
 
-    while (true) {
-      if (option === '1') {
-        console.log('Itens disponíveis para compra:');
-        console.log('1 - Espada de Ferro (50 de ouro)');
-        console.log('2 - Machado de Batalha (70 de ouro)');
-        console.log('3 - Armadura de Couro (40 de ouro)');
-        console.log('4 - Poção de Vida (30 de ouro)');
-        console.log('5 - Poção de Mana (30 de ouro)');
-        console.log('6 - Poção de Stamina (30 de ouro)');
-        console.log('7 - Voltar');
+//     while (true) {
+//       if (option === '1') {
+//         console.log('Itens disponíveis para compra:');
+//         console.log('1 - Espada de Ferro (50 de ouro)');
+//         console.log('2 - Machado de Batalha (70 de ouro)');
+//         console.log('3 - Armadura de Couro (40 de ouro)');
+//         console.log('4 - Poção de Vida (30 de ouro)');
+//         console.log('5 - Poção de Mana (30 de ouro)');
+//         console.log('6 - Poção de Stamina (30 de ouro)');
+//         console.log('7 - Voltar');
 
-        const itemChoice = write('Escolha o número do item que deseja comprar: ');
-        let selectedItem: Item | Weapon | null = null;
+//         const itemChoice = write('Escolha o número do item que deseja comprar: ');
+//         let selectedItem: Item | Weapon | null = null;
 
-        switch (itemChoice) {
-          case '1':
-            selectedItem = new Weapon('Espada de Ferro', 10, 'Espada', 50);
-            break;
-          case '2':
-            selectedItem = new Weapon('Machado de Batalha', 15, 'Machado', 70);
-            break;
-          case '3':
-            selectedItem = new Item('Armadura de Couro', 40);
-            break;
-          case '4':
-            selectedItem = new Item('Poção de Vida', 30);
-            break;
-          case '5':
-            selectedItem = new Item('Poção de Mana', 30);
-            break;
-          case '6':
-            selectedItem = new Item('Poção de Stamina', 30);
-            break;
-          case '7':
-            break;
-          default:
-            console.log('Opção inválida. Por favor, escolha um número válido.');
-            break;
-        }
+//         switch (itemChoice) {
+//           case '1':
+//             selectedItem = new Weapon('Espada de Ferro', 10, 'Espada', 50);
+//             break;
+//           case '2':
+//             selectedItem = new Weapon('Machado de Batalha', 15, 'Machado', 70);
+//             break;
+//           case '3':
+//             selectedItem = new Item('Armadura de Couro', 40);
+//             break;
+//           case '4':
+//             selectedItem = new Item('Poção de Vida', 30);
+//             break;
+//           case '5':
+//             selectedItem = new Item('Poção de Mana', 30);
+//             break;
+//           case '6':
+//             selectedItem = new Item('Poção de Stamina', 30);
+//             break;
+//           case '7':
+//             break;
+//           default:
+//             console.log('Opção inválida. Por favor, escolha um número válido.');
+//             break;
+//         }
 
-        if (selectedItem instanceof Weapon) {
-          if (this.creator.gold >= selectedItem.value) {
-            this.creator.gold -= selectedItem.value;
-            this.util.inventory.addItem(selectedItem);
-            console.log(`Você comprou ${selectedItem.name} por ${selectedItem.value} de ouro`);
-          }
-        } else if (selectedItem instanceof Item) {
-          if (this.creator.gold >= selectedItem.value) {
-            this.creator.gold -= selectedItem.value;
-            this.util.inventory.addItem(selectedItem);
-            console.log(`Você comprou ${selectedItem.name} por ${selectedItem.value} de ouro`);
-          }
-        }
-        // Após a compra, você pode retornar ao menu principal ou continuar o loop, dependendo da lógica do seu jogo
-        break; // Encerrando o loop depois da compra, mas você pode continuar caso deseje
-      } else if (option === '2') {
-        console.log('Itens disponíveis para venda:');
-        console.log('1 - Espada de Ferro (10 de ouro)');
-        console.log('2 - Machado de Batalha (15 de ouro)');
-        console.log('3 - Armadura de Couro (20 de ouro)');
-        console.log('4 - Poção de Vida (15 de ouro)');
-        console.log('5 - Poção de Mana (15 de ouro)');
-        console.log('6 - Poção de Stamina (15 de ouro)');
-        console.log('7 - Voltar');
+//         if (selectedItem instanceof Weapon) {
+//           if (this.creator.gold >= selectedItem.value) {
+//             this.creator.gold -= selectedItem.value;
+//             this.util.inventory.addItem(selectedItem);
+//             console.log(`Você comprou ${selectedItem.name} por ${selectedItem.value} de ouro`);
+//           }
+//         } else if (selectedItem instanceof Item) {
+//           if (this.creator.gold >= selectedItem.value) {
+//             this.creator.gold -= selectedItem.value;
+//             this.util.inventory.addItem(selectedItem);
+//             console.log(`Você comprou ${selectedItem.name} por ${selectedItem.value} de ouro`);
+//           }
+//         }
+//         // Após a compra, você pode retornar ao menu principal ou continuar o loop, dependendo da lógica do seu jogo
+//         break; // Encerrando o loop depois da compra, mas você pode continuar caso deseje
+//       } else if (option === '2') {
+//         console.log('Itens disponíveis para venda:');
+//         console.log('1 - Espada de Ferro (10 de ouro)');
+//         console.log('2 - Machado de Batalha (15 de ouro)');
+//         console.log('3 - Armadura de Couro (20 de ouro)');
+//         console.log('4 - Poção de Vida (15 de ouro)');
+//         console.log('5 - Poção de Mana (15 de ouro)');
+//         console.log('6 - Poção de Stamina (15 de ouro)');
+//         console.log('7 - Voltar');
 
-        const itemChoice = write('Escolha o número do item que deseja vender: ');
-        let selectedItem: Item | Weapon | null = null;
+//         const itemChoice = write('Escolha o número do item que deseja vender: ');
+//         let selectedItem: Item | Weapon | null = null;
 
-        switch (itemChoice) {
-          case '1':
-            selectedItem = new Weapon('Espada de Ferro', 10, 'Espada', 50);
-            break;
-          case '2':
-            selectedItem = new Weapon('Machado de Batalha', 15, 'Machado', 70);
-            break;
-          case '3':
-            selectedItem = new Item('Armadura de Couro', 40);
-            break;
-          case '4':
-            selectedItem = new Item('Poção de Vida', 30);
-            break;
-          case '5':
-            selectedItem = new Item('Poção de Mana', 30);
-            break;
-          case '6':
-            selectedItem = new Item('Poção de Stamina', 30);
-            break;
-          case '7':
-            break;
-          default:
-            console.log('Opção inválida. Por favor, escolha um número válido.');
-            break;
-        }
+//         switch (itemChoice) {
+//           case '1':
+//             selectedItem = new Weapon('Espada de Ferro', 10, 'Espada', 50);
+//             break;
+//           case '2':
+//             selectedItem = new Weapon('Machado de Batalha', 15, 'Machado', 70);
+//             break;
+//           case '3':
+//             selectedItem = new Item('Armadura de Couro', 40);
+//             break;
+//           case '4':
+//             selectedItem = new Item('Poção de Vida', 30);
+//             break;
+//           case '5':
+//             selectedItem = new Item('Poção de Mana', 30);
+//             break;
+//           case '6':
+//             selectedItem = new Item('Poção de Stamina', 30);
+//             break;
+//           case '7':
+//             break;
+//           default:
+//             console.log('Opção inválida. Por favor, escolha um número válido.');
+//             break;
+//         }
 
-        if (selectedItem instanceof Weapon && this.util.inventory.hasItem(selectedItem)) {
-          this.creator.gold += selectedItem.value;
-          this.util.inventory.removeItem(selectedItem);
-          console.log(`Você vendeu ${selectedItem.name} por ${selectedItem.value} de ouro`);
-        } else if (selectedItem instanceof Item && this.util.inventory.hasItem(selectedItem)) {
-          this.creator.gold += selectedItem.value;
-          this.util.inventory.removeItem(selectedItem);
-          console.log(`Você vendeu ${selectedItem.name} por ${selectedItem.value} de ouro`);
-        }
-        // Após a venda, você pode retornar ao menu principal ou continuar o loop, dependendo da lógica do seu jogo
-        const option = write('Você deseja voltar ao menu principal? (S/N): ').toLowerCase();
-        if (option === 's' || option === 'y' || option === 'sim' || option === 'yes') {
-          break;
-        } else if (option === 'n' || option === 'não' || option === 'nao' || option === 'no') {
-          continue;
-        }
-        // Encerrando o loop depois da venda, mas você pode continuar caso deseje
-        break;
-      } else if (option === '3') {
-        // Lógica para a opção "Melhorar equipamento"
-        console.log('Itens disponíveis para melhorar:');
-        this.util.inventory.items.map((item, index) => {
-          console.log(`${index} - ${item.name} | Valor: ${item.value} de ouro`);
-        });
-        // Implemente a lógica para melhorar equipamentos aqui
-        const itemChoice = write('Escolha o número do item que deseja melhorar: ');
-        const selectedItem = this.util.inventory.items[parseInt(itemChoice)];
-        if(selectedItem instanceof Weapon) {
-          if (this.creator.gold >= selectedItem.value) {
-            this.creator.gold -= selectedItem.value;
-            selectedItem.increaseDamage(5)
-            console.log(`Você melhorou ${selectedItem.name} por ${selectedItem.value} de ouro`);
-            selectedItem.increaveValue(10)
-          } else {
-            console.log('Você não tem ouro suficiente');
-          }
-        }
-        // Após melhorar o equipamento, você pode retornar ao menu principal ou continuar o loop, dependendo da lógica do seu jogo
-      } else if (option === '4') {
-        console.log('Até mais! Volte sempre à loja do ferreiro.');
-        break; // Saindo do loop quando o usuário escolhe "Sair"
-      } else {
-        console.log('Opção inválida. Por favor, escolha uma opção válida.');
-        option = write('Digite a opção: '); // Solicitando novamente a opção
-      }
-    }
-  }
-}
+//         if (selectedItem instanceof Weapon && this.util.inventory.hasItem(selectedItem)) {
+//           this.creator.gold += selectedItem.value;
+//           this.util.inventory.removeItem(selectedItem);
+//           console.log(`Você vendeu ${selectedItem.name} por ${selectedItem.value} de ouro`);
+//         } else if (selectedItem instanceof Item && this.util.inventory.hasItem(selectedItem)) {
+//           this.creator.gold += selectedItem.value;
+//           this.util.inventory.removeItem(selectedItem);
+//           console.log(`Você vendeu ${selectedItem.name} por ${selectedItem.value} de ouro`);
+//         }
+//         // Após a venda, você pode retornar ao menu principal ou continuar o loop, dependendo da lógica do seu jogo
+//         const option = write('Você deseja voltar ao menu principal? (S/N): ').toLowerCase();
+
+//         if (option === 's' || option === 'y' || option === 'sim' || option === 'yes') {
+//           break;
+//         } else if (option === 'n' || option === 'não' || option === 'nao' || option === 'no') {
+//           continue;
+//         }
+//         // Encerrando o loop depois da venda, mas você pode continuar caso deseje
+//         break;
+//       } else if (option === '3') {
+//         // Lógica para a opção "Melhorar equipamento"
+//         console.log('Itens disponíveis para melhorar:');
+//         this.util.inventory.items.map((item, index) => {
+//           console.log(`${index} - ${item.name} | Valor: ${item.value} de ouro`);
+//         });
+//         // Implemente a lógica para melhorar equipamentos aqui
+//         const itemChoice = write('Escolha o número do item que deseja melhorar: ');
+//         const selectedItem = this.util.inventory.items[parseInt(itemChoice)];
+//         if (selectedItem instanceof Weapon) {
+//           if (this.creator.gold >= selectedItem.value) {
+//             this.creator.gold -= selectedItem.value;
+//             selectedItem.increaseDamage(5);
+//             console.log(`Você melhorou ${selectedItem.name} por ${selectedItem.value} de ouro`);
+//             selectedItem.increaveValue(10);
+//           } else {
+//             console.log('Você não tem ouro suficiente');
+//           }
+//         }
+//         // Após melhorar o equipamento, você pode retornar ao menu principal ou continuar o loop, dependendo da lógica do seu jogo
+//       } else if (option === '4') {
+//         console.log('Até mais! Volte sempre à loja do ferreiro.');
+//         break; // Saindo do loop quando o usuário escolhe "Sair"
+//       } else {
+//         console.log('Opção inválida. Por favor, escolha uma opção válida.');
+//         option = write('Digite a opção: '); // Solicitando novamente a opção
+//       }
+//     }
+//   }
+// }
