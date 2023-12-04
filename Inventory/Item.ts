ab45475f69c2f95b4784eff180690738436a7cf5
Murilo <dixmurilox@gmail.com>
2023-12-04 18:57:53 -0300

    Feat healling item

--- Inventory/Item.ts
+++ Inventory/Item.ts
@@ -1,3 +1,17 @@
+import { Creator } from "../Creator";
+//Item generico
 export class Item {
   constructor(public name: string, public value: number) {}
+}
+
+// Item de cura
+
+export class HealingItem extends Item {
+  constructor(name: string, price: number, public heal: number) {
+    super(name, price);
+  }
+
+  useItem(creator: Creator) {
+    creator.stats.health += this.heal;
+  }
 }
